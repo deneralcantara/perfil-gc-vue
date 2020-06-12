47e70ccf44b692000f187e9c2ebecbfcb9c5e0ff
Dener Alcâtara <dener.alcantara@garnon.me>
2020-06-12 01:21:42 -0300

    commit final conexao heroku

--- env.js
+++ env.js
@@ -1,4 +1,5 @@
 export const env = {
-    HOST_API : "http://192.168.1.4:8000/api", // API URL
+    // HOST_API : "http://192.168.1.4:8000/api", // API URL
+    HOST_API: "https://evening-lowlands-14161.herokuapp.com/api"
 };
 
